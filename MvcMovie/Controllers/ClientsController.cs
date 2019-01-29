54b6488478e8a5c17a2f6be092e569b5097bf6ae
Xavi678 <xavisp6@gmail.com>
2019-01-29 19:53:02 +0100

    e

---
+++ MvcMovie/Controllers/ClientsController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private ClientDBContext db = new ClientDBContext();
+
+        // GET: Clients
+        public ActionResult Index(string searchString, string nom)
+        {
+            var clients = from c in db.Clients select c;
+
+           
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                if (Regex.Matches(searchString, @"[a-zA-Z]").Count!=-1)
+                {
+                    
+                    searchString = searchString.Substring( 0,searchString.Length-1);
+                }
+                clients = clients.Where(s => s.NIF.StartsWith(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(nom))
+            {
+                clients = clients.Where(s => s.Nom.Contains(nom) || s.Cognom.Contains(nom));
+            }
+
+            return View(clients);
+        }
+
+        // GET: Clients/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // GET: Clients/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clients/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NIF,Nom,Cognom,DataNaixement,Carrer,Numero,Poblacio,Provincia,CP,Tlf,Correu,Aut")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clients.Add(client);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(client);
+        }
+
+        // GET: Clients/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // POST: Clients/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NIF,Nom,Cognom,DataNaixement,Carrer,Numero,Poblacio,Provincia,CP,Tlf,Correu,Aut")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(client).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(client);
+        }
+
+        // GET: Clients/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // POST: Clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Client client = db.Clients.Find(id);
+            db.Clients.Remove(client);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
